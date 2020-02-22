96d3f01fb37d05a4f854cb609fa1e4b5aa1fa278
Soham Manoli <msoham123@gmail.com>
2020-02-23 01:31:23 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:login_dash_animation/screens/homeScreen.dart';
 
-void main() => runApp(MyApp());
+//Device Preview
+void main() => runApp(DevicePreview(builder: (context) => MyApp()));
+
+//void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.of(context).locale,
+      builder: DevicePreview.appBuilder,
+      //activate for device preview
       debugShowCheckedModeBanner: false,
       title: 'Surfing',
       theme: ThemeData(
